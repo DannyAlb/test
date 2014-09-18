9cece99af183b29a5b2505ce90af3e346c613756
DannyAlb <danny.albocher@gmail.com>
2014-09-18 23:39:38 +0300

    test2

--- TestProject/src/Main.java
+++ TestProject/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		if (args.length == 0)
-			System.out.println("no params2");
+			System.out.println("no params");
 
 		// XmppDebugger debugger = new XmppDebugger("debugger.one@gmail.com", "debugger123", "danny.albocher@gmail.com");
 		// debugger.readInitialBreakpoints();

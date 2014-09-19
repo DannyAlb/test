a1606698411b5adf12f8d01fa1b50b76089a4f6b
DannyAlb <danny.albocher@gmail.com>
2014-09-20 00:17:51 +0300

    another commit of the master

--- TestProject/src/Main.java
+++ TestProject/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		if (args.length == 0)
-			System.out.println("I'm another commit of the master");
+			System.out.println("I'm another commit of the master. and another!");
 
 		
 		

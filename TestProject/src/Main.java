97d0522b6baadcef4485e1ed28c73defc4238fdb
DannyAlb <danny.albocher@gmail.com>
2014-09-20 00:16:48 +0300

    master commit after I had a branch

--- TestProject/src/Main.java
+++ TestProject/src/Main.java
@@ -3,8 +3,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		if (args.length == 0)
-			System.out.println("no params4");
+			System.out.println("I'm another commit of the master");
 
+		
+		
 		// XmppDebugger debugger = new XmppDebugger("debugger.one@gmail.com", "debugger123", "danny.albocher@gmail.com");
 		// debugger.readInitialBreakpoints();
 		for (String arg : args) {

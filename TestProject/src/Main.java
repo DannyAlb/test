85661451d449d43dd84d2efcb395429d7b764e9e
DannyAlb <danny.albocher@gmail.com>
2014-09-20 00:19:10 +0300

    in a branch, another commit

--- TestProject/src/Main.java
+++ TestProject/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		if (args.length == 0)
-			System.out.println("I'm in another commit in a branch!");
+			System.out.println("I'm in another commit in a branch! and another!");
 
 		// XmppDebugger debugger = new XmppDebugger("debugger.one@gmail.com", "debugger123", "danny.albocher@gmail.com");
 		// debugger.readInitialBreakpoints();
